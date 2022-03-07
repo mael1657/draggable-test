1cc5cee0613f9b29a24e0d77daf467a1eb4e7895
Lee JuHyeon <jhmael013@gmail.com>
2022-03-07 12:28:14 +0900

    chore: draggable package

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Draggable from "react-draggable";
 import { Square } from "../components/Module";
 
 const Main = () => {
   return (
     <div className="wrap">
       <div className="content">
-        <Square content={"네모박스"} />
+        <Draggable>
+          <div>ㅎㅇㅎㅇㅎㅇ</div>
+        </Draggable>
+        <Draggable>
+          <Square content={"네모박스"} />
+        </Draggable>
       </div>
     </div>
   );
