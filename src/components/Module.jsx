61a927f982d42dd6d2df93356f87438ae05c40b0
Lee JuHyeon <jhmael013@gmail.com>
2022-03-12 18:49:25 +0900

    [test] draggble test

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import Draggable from "react-draggable";
 
 export const Square = (props) => {
+  const nodeRef = useRef(null);
   return (
     <>
-      <Draggable>
-        <SquareStyledBox>{props.data}</SquareStyledBox>;
+      <Draggable nodeRef={nodeRef}>
+        <SquareStyledBox ref={nodeRef}>{props.children}</SquareStyledBox>
       </Draggable>
     </>
   );
@@ -16,4 +17,5 @@ const SquareStyledBox = styled.div`
   width: 100px;
   height: 100px;
   border: 1px solid #e3e3e3;
+  background-color: skyblue;
 `;
